4af89051cf390579dbb1d7f7e3c1419b92a27395
Nguyen Duc Long <61280727+DucLongDTH@users.noreply.github.com>
2023-05-25 10:18:56 +0700

    Update fluttertoast.dart

--- lib/fluttertoast.dart
+++ lib/fluttertoast.dart
@@ -366,50 +366,50 @@ class _ToastStateFul extends StatefulWidget {
 /// State for [_ToastStateFul]
 class ToastStateFulState extends State<_ToastStateFul>
     with SingleTickerProviderStateMixin {
-  /// Start the showing animations for the toast
-  showIt() {
-    _animationController!.forward();
-  }
+//   /// Start the showing animations for the toast
+//   showIt() {
+//     _animationController!.forward();
+//   }
 
-  /// Start the hidding animations for the toast
-  hideIt() {
-    _animationController!.reverse();
-    _timer?.cancel();
-  }
+//   /// Start the hidding animations for the toast
+//   hideIt() {
+//     _animationController!.reverse();
+//     _timer?.cancel();
+//   }
 
-  /// Controller to start and hide the animation
-  AnimationController? _animationController;
-  late Animation _fadeAnimation;
+//   /// Controller to start and hide the animation
+//   AnimationController? _animationController;
+//   late Animation _fadeAnimation;
 
-  Timer? _timer;
+//   Timer? _timer;
 
   @override
   void initState() {
-    _animationController = AnimationController(
-      vsync: this,
-      duration: widget.fadeDuration,
-    );
-    _fadeAnimation =
-        CurvedAnimation(parent: _animationController!, curve: Curves.easeIn);
+//     _animationController = AnimationController(
+//       vsync: this,
+//       duration: widget.fadeDuration,
+//     );
+//     _fadeAnimation =
+//         CurvedAnimation(parent: _animationController!, curve: Curves.easeIn);
     super.initState();
 
-    showIt();
-    _timer = Timer(widget.duration, () {
-      hideIt();
-    });
+//     showIt();
+//     _timer = Timer(widget.duration, () {
+//       hideIt();
+//     });
   }
 
   @override
   void deactivate() {
-    _timer?.cancel();
-    _animationController!.stop();
+//     _timer?.cancel();
+//     _animationController!.stop();
     super.deactivate();
   }
 
   @override
   void dispose() {
-    _timer?.cancel();
-    _animationController?.dispose();
+//     _timer?.cancel();
+//     _animationController?.dispose();
     super.dispose();
   }
 
